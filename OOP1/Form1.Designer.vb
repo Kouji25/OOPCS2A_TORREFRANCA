865c7ec1d554e94bf1ddc6c9598c3188f18f2049
ITLAB-38 <ITLAB-38@ITLAB-38>
2023-10-14 15:10:54 -0700

    Hi

---
+++ OOP1/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        txtFName = New TextBox()
+        txtLName = New TextBox()
+        txtCourse = New TextBox()
+        btnInsert = New Button()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(btnInsert)
+        GroupBox1.Controls.Add(txtCourse)
+        GroupBox1.Controls.Add(txtLName)
+        GroupBox1.Controls.Add(txtFName)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Location = New Point(35, 42)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(239, 187)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Save Records"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(22, 32)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(64, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "First Name"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(22, 64)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(63, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Last Name"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(22, 95)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(44, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Course"
+        ' 
+        ' txtFName
+        ' 
+        txtFName.Location = New Point(103, 29)
+        txtFName.Name = "txtFName"
+        txtFName.Size = New Size(100, 23)
+        txtFName.TabIndex = 2
+        ' 
+        ' txtLName
+        ' 
+        txtLName.Location = New Point(103, 61)
+        txtLName.Name = "txtLName"
+        txtLName.Size = New Size(100, 23)
+        txtLName.TabIndex = 3
+        ' 
+        ' txtCourse
+        ' 
+        txtCourse.Location = New Point(103, 92)
+        txtCourse.Name = "txtCourse"
+        txtCourse.Size = New Size(100, 23)
+        txtCourse.TabIndex = 4
+        ' 
+        ' btnInsert
+        ' 
+        btnInsert.Location = New Point(61, 130)
+        btnInsert.Name = "btnInsert"
+        btnInsert.Size = New Size(107, 23)
+        btnInsert.TabIndex = 5
+        btnInsert.Text = "Save Record"
+        btnInsert.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Private WithEvents btnInsert As Button
+    Friend WithEvents txtCourse As TextBox
+    Friend WithEvents txtLName As TextBox
+    Friend WithEvents txtFName As TextBox
+End Class
---
+++ OOP1/Module1.vb
@@ -0,0 +1,12 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module Module1
+
+    Dim con As New MySqlConnection
+    Dim reader As MySqlDataReader
+    Dim mysqlcmd As New MySqlCommand
+    Public Sub ConnectDbase()
+
+    End Sub
+
+End Module
