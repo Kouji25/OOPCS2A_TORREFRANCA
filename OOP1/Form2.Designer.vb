13224738e2b92f18c68409c1cf43d3e15559b779
ITLAB-38 <ITLAB-38@ITLAB-38>
2023-10-21 15:33:52 -0700

    333

--- OOP1/Form2.Designer.vb
+++ OOP1/Form2.Designer.vb
@@ -22,47 +22,47 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        ComboBox1 = New ComboBox()
-        Button1 = New Button()
-        DataGridView1 = New DataGridView()
-        Button2 = New Button()
-        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        cboCourse = New ComboBox()
+        btnDisplay = New Button()
+        dgvData = New DataGridView()
+        btnBack = New Button()
+        CType(dgvData, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
-        ' ComboBox1
+        ' cboCourse
         ' 
-        ComboBox1.FormattingEnabled = True
-        ComboBox1.Location = New Point(67, 101)
-        ComboBox1.Name = "ComboBox1"
-        ComboBox1.Size = New Size(214, 23)
-        ComboBox1.TabIndex = 0
+        cboCourse.FormattingEnabled = True
+        cboCourse.Location = New Point(67, 101)
+        cboCourse.Name = "cboCourse"
+        cboCourse.Size = New Size(214, 23)
+        cboCourse.TabIndex = 0
         ' 
-        ' Button1
+        ' btnDisplay
         ' 
-        Button1.Location = New Point(368, 101)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(132, 40)
-        Button1.TabIndex = 1
-        Button1.Text = "Button1"
-        Button1.UseVisualStyleBackColor = True
+        btnDisplay.Location = New Point(368, 101)
+        btnDisplay.Name = "btnDisplay"
+        btnDisplay.Size = New Size(132, 40)
+        btnDisplay.TabIndex = 1
+        btnDisplay.Text = "Display"
+        btnDisplay.UseVisualStyleBackColor = True
         ' 
-        ' DataGridView1
+        ' dgvData
         ' 
-        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView1.Location = New Point(67, 147)
-        DataGridView1.Name = "DataGridView1"
-        DataGridView1.RowTemplate.Height = 25
-        DataGridView1.Size = New Size(436, 234)
-        DataGridView1.TabIndex = 2
+        dgvData.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgvData.Location = New Point(67, 147)
+        dgvData.Name = "dgvData"
+        dgvData.RowTemplate.Height = 25
+        dgvData.Size = New Size(436, 234)
+        dgvData.TabIndex = 2
         ' 
-        ' Button2
+        ' btnBack
         ' 
-        Button2.Location = New Point(254, 421)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(75, 23)
-        Button2.TabIndex = 3
-        Button2.Text = "Button2"
-        Button2.UseVisualStyleBackColor = True
+        btnBack.Location = New Point(254, 421)
+        btnBack.Name = "btnBack"
+        btnBack.Size = New Size(75, 23)
+        btnBack.TabIndex = 3
+        btnBack.Text = "Home"
+        btnBack.UseVisualStyleBackColor = True
         ' 
         ' Form2
         ' 
@@ -70,18 +70,18 @@ Partial Class Form2
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(653, 502)
         ControlBox = False
-        Controls.Add(Button2)
-        Controls.Add(DataGridView1)
-        Controls.Add(Button1)
-        Controls.Add(ComboBox1)
+        Controls.Add(btnBack)
+        Controls.Add(dgvData)
+        Controls.Add(btnDisplay)
+        Controls.Add(cboCourse)
         Name = "Form2"
         Text = "Form2"
-        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        CType(dgvData, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
     End Sub
 
-    Friend WithEvents ComboBox1 As ComboBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents Button2 As Button
+    Friend WithEvents cboCourse As ComboBox
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents dgvData As DataGridView
+    Friend WithEvents btnBack As Button
 End Class
--- OOP1/Form2.vb
+++ OOP1/Form2.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form2
-
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        Form1.Show()
+        Me.Hide()
+    End Sub
 End Class
